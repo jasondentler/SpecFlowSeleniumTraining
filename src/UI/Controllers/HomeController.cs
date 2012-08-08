ebce633b6ffd37486bd7bc8eef734f60ca6a468a
Jason Dentler <jasondentler@gmail.com>
2012-08-08 13:00:32 -0500

    Add home controller

---
+++ src/UI/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public RedirectToRouteResult Index()
+        {
+            return View();
+        }
+
+    }
+}
