9c29d078fe951944bf76138361d08cbf5d7841b5
Jason Dentler <jasondentler@gmail.com>
2012-08-08 13:54:34 -0500

    Pending steps

---
+++ src/Specs/HomePageSteps.cs
@@ -0,0 +1,22 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Specs
+{
+    [Binding]
+    public class HomePageSteps
+    {
+        
+        [When(@"I visit the home page")]
+        public void WhenIVisitTheHomePage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the page redirects to the widget list")]
+        public void ThenThePageRedirectsToTheWidgetList()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
