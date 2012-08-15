e28e2f747440b63b9847477d7bf23c74544b924b
Jason Dentler <jasondentler@gmail.com>
2012-08-15 07:43:59 -0500

    Removed code for demo purposes

--- src/Specs/WidgetSteps.cs
+++ src/Specs/WidgetSteps.cs
@@ -1,142 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using OpenQA.Selenium;
-using SharpTestsEx;
-using Specs.Infrastructure;
-using TechTalk.SpecFlow;
-using UI.Models;
 
 namespace Specs
 {
-    [Binding]
-    public class WidgetSteps : WebSteps 
+    public class WidgetSteps
     {
 
-        [Given(@"I have added one widget")]
-        [Given(@"I have added a widget")]
-        public void GivenIHaveAddedAWidget()
+
+        private int GetIdFromDetailsLink(IWebElement anchorTag)
         {
-            CreateWidget();
+            var url = anchorTag.GetAttribute("href");
+            var regex = new Regex(@"/widgets/(?<id>\d+)$");
+            var sid = regex.Match(url).Groups["id"].Value;
+            return Convert.ToInt32(sid);
         }
 
-        [Given(@"I have added two widgets")]
-        public void GivenIHaveAddedTwoWidgets()
-        {
-            CreateWidget();
-            CreateWidget();
-        }
-
-        [When(@"I create a widget")]
-        public void WhenICreateAWidget()
-        {
-            CreateWidget();
-        }
-
-        [When(@"I display the widget")]
-        public void WhenIDisplayTheWidget()
-        {
-            var widget = _widgets.Single();
-            var url = string.Format("/widgets/{0}", widget.Id);
-            NavigateTo(url);
-        }
-
-        [When(@"I view the widget list")]
-        public void WhenIViewTheWidgetList()
-        {
-            NavigateTo("/widgets");
-        }
-
-        [When(@"I display a widget that doesn't exist")]
-        public void WhenIDisplayAWidgetThatDoesnTExist()
-        {
-            NavigateTo("/widgets/-1");
-        }
-
-        [Then(@"the widget details are displayed")]
-        [Then(@"the widget is displayed")]
-        public void ThenTheWidgetIsDisplayed()
-        {
-            var widget = _widgets.Single();
-
-            var expectedUrl = string.Format(@"widgets/{0}", widget.Id);
-            RelativeUrl().ToString().Should().Be.EqualTo(expectedUrl);
-
-            var form = new MvcFormHelper<WidgetDetails>(Browser);
-
-            form.Get(m => m.Name).Should().Be.EqualTo(widget.Name);
-
-            Math.Abs(form.Get(m => m.Size) - widget.Size)
-                .Should().Be.LessThan(Epsilon);
-        }
-
-        [Then(@"the error message is ""(.*)""")]
-        public void ThenTheErrorMessageIs(string expected)
-        {
-            var actual = Browser.FindElement(By.ClassName("error")).Text;
-            actual.Should().Be.EqualTo(expected);
-        }
-
-        [Then(@"two widgets are listed")]
-        public void ThenTwoWidgetsAreListed()
-        {
-            var expected = _widgets.ToDictionary(widget => widget.Id,
-                                                 widget => widget.Name);
-
-            var widgetLinks = Browser.FindElements(By.XPath("//ul[@id='widgetList']//a"));
-
-            var getIdFromUrl = new Func<string, int>(url =>
-                                                         {
-                                                             var regex = new Regex(@"/widgets/(?<id>\d+)$");
-                                                             return Convert.ToInt32(regex.Match(url).Groups["id"].Value);
-                                                         });
-
-            var actual = widgetLinks.ToDictionary(
-                link => getIdFromUrl(link.GetAttribute("href")),
-                link => link.Text);
-
-            actual.Should().Have.SameValuesAs(expected);
-        }
-
-        private readonly List<WidgetDetails> _widgets = new List<WidgetDetails>();
-        private const float Epsilon = 0.0001F;
-
-        private void CreateWidget()
-        {
-            CreateWidget(new WidgetDetails()
-                             {
-                                 Name = Guid.NewGuid().ToString(),
-                                 Size = Convert.ToSingle(new Random().NextDouble()*50)
-                             });
-        }
-
-        private void CreateWidget(WidgetDetails widget)
-        {
-            NavigateTo("/widgets/create");
-            var form = new MvcFormHelper<WidgetDetails>(Browser);
-            form.Set(m => m.Name, widget.Name);
-            form.Set(m => m.Size, widget.Size);
-            form.Submit(m => m.Name);
-
-            //var errors = Browser.FindElements(By.ClassName("error"));
-            //if (errors != null &&  errors.Any())
-            //{
-            //    Console.WriteLine("Widget creation failed with error(s):");
-            //    errors.ToList().ForEach(e => Console.WriteLine(e.Text));
-            //    return;
-            //}
-
-            var match = new Regex(@"^widgets/(?<id>\d+)$").Match(RelativeUrl().ToString());
-            if (match.Success)
-            {
-                widget.Id = Convert.ToInt32(match.Groups["id"].Value);
-                _widgets.Add(widget);
-                Console.WriteLine("Widget {0} created", widget.Id);
-            } else
-            {
-                Console.WriteLine("Widget creation failed.");
-            }
-        }
     }
 }
